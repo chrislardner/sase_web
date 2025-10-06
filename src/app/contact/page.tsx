44508b5c5ae2899a77df39b7c9b677e2bc64dcf8
chrislardner <chrislardnercl@gmail.com>
2025-10-06 16:14:04 -0400

    Small tweaks to layout, contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,6 +9,14 @@ export default function ContactPage() {
 
             <div className="mt-6 flex flex-col sm:flex-row items-center justify-center gap-3">
                 <a
+                    href="https://instagram.com/saserhit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-icon w-full sm:w-auto inline-flex items-center gap-2"
+                >
+                    <FaInstagram /> <span>Instagram</span>
+                </a>
+                <a
                     href="https://rosehulman.campusgroups.com/feeds?type=club&type_id=35480&tab=about"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -21,14 +29,6 @@ export default function ContactPage() {
                     className="btn btn-icon w-full sm:w-auto inline-flex items-center gap-2"
                 >
                     <FaEnvelope /> <span>Email Us</span>
-                </a>
-                <a
-                    href="https://instagram.com/saserhit"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-icon w-full sm:w-auto inline-flex items-center gap-2"
-                >
-                    <FaInstagram /> <span>Instagram</span>
                 </a>
                 <a
                     href="https://github.com/chrislardner/sase_web"
